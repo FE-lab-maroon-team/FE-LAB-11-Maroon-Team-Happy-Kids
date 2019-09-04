b1004ffc586ba2468c5751417c5163cf2cd92da6
VolodymyrProdyus <volpro86@gmail.com>
2019-09-04 23:51:18 +0300

    Change import {Events} from './events'

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Homepage} from './homepage';
-import {Events} from './events/events';
+import {Events} from './events';
 import {Pdp} from './pdp';
 
 function App() {
