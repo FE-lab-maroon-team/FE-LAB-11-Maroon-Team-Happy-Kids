b563a97d105c71e1a4728c6e0a00adcdddbe0de0
Yurii Ilnytskyi <yurii.ilnytskyi@gmail.com>
2019-09-04 17:40:25 +0300

    Test commit

--- my-app/src/pages/events/events.component.js
+++ my-app/src/pages/events/events.component.js
@@ -1,1 +1,9 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+export default class Event extends Component {
+  render () {
+    return (
+      <div>Hello world!</div>
+    )
+  }
+}
