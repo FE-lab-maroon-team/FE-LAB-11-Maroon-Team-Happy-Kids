a87a47f12f1d0b2097f21b3b5f7212bbd575523a
Nazarkon <nazarmontsibovych@gmail.com>
2019-09-10 22:21:44 +0300

    Add button

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,33 +5,37 @@ import { Events } from './pages/events';
 import { Homepage } from './pages/homepage';
 import { Pdp } from './pages/pdp';
 import { Header } from './public-components/header'
+import { Button } from './public-components/button';
 // import { MainPage } from './pages';
 
 function AppRouter(){
 
     return (
+        <>
         <Header />
-        // <Router>
-        //     <div>
-        //         <nav>
-        //             <ul>
-        //                 <li>
-        //                     <Link to='/'>Home</Link>
-        //                 </li>
-        //                 <li>
-        //                     <Link to='/events'>Events</Link>
-        //                 </li>
-        //                 <li>
-        //                     <Link to='/pdp'>Pdp</Link>
-        //                 </li>
-        //             </ul>
-        //         </nav>
+        <Button text="Button"/>
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/events'>Events</Link>
+                        </li>
+                        <li>
+                            <Link to='/pdp'>Pdp</Link>
+                        </li>
+                    </ul>
+                </nav>
 
-        //         <Route path="/" exact component ={Homepage}/>
-        //         <Route path="/events/" exact component ={Events}/>
-        //         <Route path="/pdp/" exact component ={Pdp}/>
-        //     </div>
-        // </Router>
+                <Route path="/" exact component ={Homepage}/>
+                <Route path="/events/" exact component ={Events}/>
+                <Route path="/pdp/" exact component ={Pdp}/>
+            </div>
+        </Router>
+        </>
     )
 }
 ReactDOM.render(<AppRouter />,document.querySelector('#root')
