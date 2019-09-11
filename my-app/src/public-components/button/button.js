6be1d73412e05aeccd8ddbb04995d721d4dfb559
Nazar Montsibovych <37847409+Nazarkon@users.noreply.github.com>
2019-09-11 19:49:04 +0300

    Update button.js

--- my-app/src/public-components/button/button.js
+++ my-app/src/public-components/button/button.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './button.css';
 
-export const Button = ({text,handler}) =>{
+export const Button = ({text,onClick}) =>{
 
     return(
-        <button  className="button" onClick={handler}>{text}</button>
+        <button  className="button" onClick={onClick}>{text}</button>
     )
 }
