a2d6255262d6b384e5df92fcf7c8012ac0a60dfe
Nazarkon <nazarmontsibovych@gmail.com>
2019-09-14 17:20:03 +0300

    Version 3.0

--- my-app/src/pages/pdp/AboutMe/aboutPrf.js
+++ my-app/src/pages/pdp/AboutMe/aboutPrf.js
@@ -4,18 +4,21 @@ import './aboutPrf.scss';
 export class AboutMeComponent extends React.Component{
      
       render(){
-         return(<div className="container_aboutMe">
-             <h1 className="aboutMe_title">Про мене</h1>
+         return(
+         <div className="container_aboutMe">
+                    <h2 className="aboutMe_title">Про мене</h2>
+             <div className="aboutMe_container">
              <div className="aboutMe_photo">
                <img src="" alt="children_photo"/>
              </div>
              <div className="aboutMe_description">
                 <ul className="aboutMe_items">
-                   <li><span>Ім'я</span></li>
-                   <li><span>Прізвище</span></li>
-                   <li><span>Вік</span></li>
-                   <li><span>Про мене:</span></li>
+                   <li><span className="items_name">Ім'я:</span></li>
+                   <li><span className="items_surname">Прізвище: </span></li>
+                   <li><span className="items_age">Вік: </span></li>
+                   <li><span className="items_description">Про мене: </span></li>
                 </ul>
+             </div>
              </div>
          </div>
          )
