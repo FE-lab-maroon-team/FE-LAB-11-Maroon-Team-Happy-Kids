1bbeb15524fa5ff7e1f3be4fbbafe921083556bf
VolodymyrProdyus <volpro86@gmail.com>
2019-09-15 21:15:04 +0300

    Add social, text and lines

--- my-app/src/public-components/footer/footer.js
+++ my-app/src/public-components/footer/footer.js
@@ -47,30 +47,63 @@ export class Footer extends Component {
 
   render() {
   return (
-        <div className={classnames("footer", {
-          "footer_hidden": this.state.visible })}>
-      <div className="footer_logo">
-        <div>
-          {new Date().getFullYear()} &#9400;: 
-          <Textlink className="footer_logo_text" name=' nadiya.com' path='https://www.nadiya.com' />
+    <div className={classnames("footer", {
+      "footer_hidden": this.state.visible })}>
+        <div className="footer_logo_social">
+          <div className="footer_logo">
+          <div>
+            {new Date().getFullYear()} &#9400;: 
+            <Textlink className="footer_logo_text" name=' nadiya.com' path='https://www.nadiya.com' />
+          </div>
+            <div className="footer_logo_img"> </div>
+          </div>
+
+          <div className="footer_social">
+              <p>Social</p>
+            <ul>
+              <li><Icon type="facebook" style={{ fontSize: '30px' }}/></li>
+              <li><Icon type="instagram" style={{ fontSize: '30px' }}/></li>
+              <li><Icon type="twitter" style={{ fontSize: '30px' }}/></li>
+              <li><Icon type="skype" style={{ fontSize: '30px' }}/></li>
+            </ul>
+          </div>
         </div>
-        <div className="footer_logo_img"> </div>
-      </div>
+
+        <div class="or-spacer-vertical left">
+          <div class="mask"></div>
+        </div>
+        <div className="footer_text_about">
+          <a>
+          Lorem Ipsum - це текст-"риба", 
+          що використовується в друкарстві та 
+          дизайні. Lorem Ipsum є, фактично, стандартною 
+          "рибою" аж з XVI сторіччя, коли невідомий друкар взяв шрифтову 
+         гранку та склав на ній підбірку зразків шрифтів. 
+          "Риба" не тільки успішно пережила п'ять століть, 
+          але й прижилася в електронному верстуванні, 
+          залишаючись по суті незмінною.
+          </a>
+        </div>
+
+        <div class="or-spacer-vertical right">
+          <div class="mask"></div>
+        </div>
+      
 
       <div className="footer_contacts_links">
         <ul className="footer_contacts">
           <li><Icon className="footer_contacts_icons" type="phone" style={{ fontSize: '30px', color: '#DA3E44' }} theme="outlined" />
-            <Textlink name='Phone' path='#' /></li>
+            <Textlink name='+38(097)1234567' path='' /></li>
           <li><Icon className="footer_contacts_icons" type="mail" style={{ fontSize: '30px', color: '#DA3E44' }} theme="outlined" />
-          <Textlink name='Mail' path='#' /></li>
+          <Textlink name='nadiya.com' path='' /></li>
           <li><Icon className="footer_contacts_icons" type="home" style={{ fontSize: '30px', color: '#DA3E44' }} theme="outlined" />
-          <Textlink name='Adress' path='#' /></li>
+          <Textlink name='м.Львів, вул.Центральна,1' path='' /></li>
         </ul>
 
         <ul className="footer_links">
-          <li><Textlink name='Home' path='../../pages/homepage' /></li>
-          <li><Textlink name='Events' path='../../pages/events' /></li>
-          <li><Textlink name='Pdp' path='../../pages/pdp' /></li>
+          <li><Textlink name='Домашня' path='../../pages/homepage' /></li>
+          <li><Textlink name='Події' path='../../pages/events' /></li>
+          <li><Textlink name='Деталі' path='../../pages/pdp' /></li>
         </ul>
       </div>
  
