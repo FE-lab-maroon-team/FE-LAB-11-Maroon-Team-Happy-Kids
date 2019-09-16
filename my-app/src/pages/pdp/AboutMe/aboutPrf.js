43d242827586db9066e60bf311e2f90023c18458
Nazarkon <nazarmontsibovych@gmail.com>
2019-09-16 23:09:08 +0300

    Version 3.0

--- my-app/src/pages/pdp/AboutMe/aboutPrf.js
+++ my-app/src/pages/pdp/AboutMe/aboutPrf.js
@@ -1,26 +1,56 @@
 import React from 'react';
 import './aboutPrf.scss';
+import { db } from '../../../firebase-config';
 
 export class AboutMeComponent extends React.Component{
-     
-      render(){
-         return(
+
+   state = {
+      allUsers: []
+    }
+   
+     componentDidMount() {
+       const allUsers = db.collection('users');
+       const usersData = [];
+       allUsers.get().then(snapshot => {
+         snapshot.forEach(doc => {
+           usersData.push(doc.data());
+         })
+         this.setState({
+           allUsers: usersData
+         })
+       })
+     }
+
+     renderUser(users) {
+      return users.map(user => {
+        const {name, surname, photoUrl,description,birthday} = user;
+        console.log(user)
+
+        return(
          <div className="container_aboutMe">
                     <h2 className="aboutMe_title">Про мене</h2>
              <div className="aboutMe_container">
              <div className="aboutMe_photo">
-               <img src="" alt="children_photo"/>
+               <img src={photoUrl} alt="children_photo"/>
              </div>
              <div className="aboutMe_description">
                 <ul className="aboutMe_items">
-                   <li><span className="items_name">Ім'я:</span></li>
-                   <li><span className="items_surname">Прізвище: </span></li>
-                   <li><span className="items_age">Вік: </span></li>
-                   <li><span className="items_description">Про мене: </span></li>
+                   <li><span className="items_name">Ім'я:  {name}</span></li>
+                   <li><span className="items_surname">Прізвище:  {surname}</span></li>
+                   <li><span className="items_age">Рік народження:  {birthday}</span></li>
+                   <li><span className="items_description">Про мене:  {description}</span></li>
                 </ul>
              </div>
              </div>
          </div>
+         )
+      })
+    }
+
+      render(){
+
+         return( 
+         <div>{this.renderUser(this.state.allUsers)}</div>
          )
       }
 
