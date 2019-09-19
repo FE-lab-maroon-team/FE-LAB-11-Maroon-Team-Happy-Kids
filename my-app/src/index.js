1ebfbb7a79b61eee88f26c0ad289bf263a7b021e
Nazarkon <nazarmontsibovych@gmail.com>
2019-09-19 21:31:17 +0300

    Delete some useless files

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { Events } from './pages/events';
-import { Profile } from './pages/profile';
 import { Footer } from './public-components/footer';
 import { Header } from './public-components/header'
 import styles from './index.module.scss';
-import { Homepage } from './pages/homepage';
 // import { Button } from './public-components/button';
 
 function AppRouter(){
