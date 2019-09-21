8546b9f2ade79a0c59481edaa4dfd43f2b6970b2
Alina_ <alina.kuzzzovkova@gmail.com>
2019-09-21 12:50:54 +0300

    final fixes to events block on Homepage

--- my-app/src/pages/homepage/events/events.component.js
+++ my-app/src/pages/homepage/events/events.component.js
@@ -1,41 +1,41 @@
 import React from "react";
-import { Button } from '../../../public-components/button';
-import { Spinner } from '../../../public-components/spinner';
-import styles from './events.module.scss';
-import { convertToDate } from '../../../libs/helpers/convertToDate';
-import { useEvents } from '../../../libs/helpers/useEvents';
+import { Button } from "../../../public-components/button";
+import { Spinner } from "../../../public-components/spinner";
+import styles from "./events.module.scss";
+import { convertToDate } from "../../../libs/helpers/convertToDate";
+import { useEvents } from "../../../libs/helpers/useEvents";
 
 function Events() {
-    const events = useEvents();
-    const [firstEvent, ...other] = events;
+  const events = useEvents();
+  const [firstEvent, ...other] = events;
 
-    if (firstEvent === undefined) {
-        return <Spinner />
-    } else {
-        return (
-            <div className={styles.eventBlock}>
-                <div className={styles.eventImage}>
-                    <img src={firstEvent.imageUrl} alt='charity meeting'></img>
-                </div>
-                <div className={styles.eventDescription}>
-                    <div>
-                        <h2>{firstEvent.name}</h2>
-                        <div>
-                            <p>Коли: <strong>{convertToDate(firstEvent.date.seconds)}</strong></p>
-                            <p>Де: <strong>{firstEvent.address}</strong></p>
-                        </div>
-                        <p>{firstEvent.description}</p>
-                        <div className={styles.button}>
-                            {firstEvent.donated && (
-                                <Button text='Детальніше...' />
-                            )}
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  if (firstEvent === undefined) {
+    return <Spinner />;
+  }
+  return (
+    <div className={styles.eventBlock}>
+      <div className={styles.eventImage}>
+        <img src={firstEvent.imageUrl} alt="charity meeting"></img>
+      </div>
+      <div className={styles.eventDescription}>
+        <div>
+          <h2>{firstEvent.name}</h2>
+          <div>
+            <p>
+              Коли: <strong>{convertToDate(firstEvent.date.seconds)}</strong>
+            </p>
+            <p>
+              Де: <strong>{firstEvent.address}</strong>
+            </p>
+          </div>
+          <p>{firstEvent.description}</p>
+          <div className={styles.button}>
+            {firstEvent.donated && <Button text="Детальніше..." />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Events
+export default Events;
