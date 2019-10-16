3c17b07f42a03c198100775fdf8b1a87d8cdfefc
Roman <romanzhydyk41@gmail.com>
2019-10-16 15:59:13 +0300

    add some changes to wishes

--- my-app/src/pages/profile/my-wishes/my-wishes.component.js
+++ my-app/src/pages/profile/my-wishes/my-wishes.component.js
@@ -3,15 +3,15 @@ import styles from './my-wishes.module.scss';
 import { db } from '../../../firebase-config';
 import { Portal } from '../../../public-components/portal/index';
 import { Button } from '../../../public-components/button';
-import {Spinner} from '../../../public-components/spinner';
+import { Spinner } from '../../../public-components/spinner';
 
 export class MyWishes extends Component {
     state = {
         user: null,
         showModal: false
     }
-    handleShowMessageClick = () => this.setState({showModal: true})
-    handleCloseModal = () => this.setState({showModal: false})
+    handleShowMessageClick = () => this.setState({ showModal: true })
+    handleCloseModal = () => this.setState({ showModal: false })
 
     componentDidMount() {
         let documentRef = db.collection('users').doc(this.props.userId);
@@ -27,41 +27,41 @@ export class MyWishes extends Component {
             });
     }
     render() {
-        if(!this.state.user){
+        if (!this.state.user) {
             return <Spinner />
         }
-        const {wishes} = this.state.user;        
+        const { wishes } = this.state.user;
         return (
             <div className={styles.mainMyWishes}>
                 <h1>Мої бажання</h1>
                 <div className={styles.myWishes}>
-                {wishes.map(({name, price, description, presentUrl, country}) =>(
-                    <div className={styles.col_4} key={price}>
-                        <img src={presentUrl} alt="Фото подарунка"></img>
-                        <div className={styles.myWishesDetails}>
-                            <h3>{name}</h3>
-                            <div className={styles.subjectDetails}>
-                                <div className={styles.myWishesText}>
-                                    <p>Ціна</p>
-                                    <p>Країна виробник</p>
+                    {wishes.map(({ name, price, description, presentUrl, country }) => (
+                        <div className={styles.col_4} key={price}>
+                            <img src={presentUrl} alt="Фото подарунка"></img>
+                            <div className={styles.myWishesDetails}>
+                                <h3>{name}</h3>
+                                <div className={styles.subjectDetails}>
+                                    <div className={styles.myWishesText}>
+                                        <p>Ціна</p>
+                                        <p>Країна виробник</p>
+                                    </div>
+                                    <div className={styles.myWishesPrice}>
+                                        <p>{price}</p>
+                                        <p>{country}</p>
+                                    </div>
                                 </div>
-                                <div className={styles.myWishesPrice}>
-                                    <p>{price}</p>
-                                    <p>{country}</p>
+                                <div className={styles.myWishesDescribe}>
+                                    <p>{description}</p>
+                                    <Button text="Подарувати" onClick={this.handleShowMessageClick} className={styles.button} />
                                 </div>
-                            </div>
-                            <div className={styles.myWishesDescribe}>
-                                <p>{description}</p>
-                                <Button text="Подарувати" onClick={this.handleShowMessageClick} className={styles.button} />
                             </div>
                         </div>
-                    </div>
                     ))}
                 </div>
-                {this.state.showModal  && (
-            <Portal onClose={this.handleCloseModal}>        
-            </Portal>
-          )}
+                {this.state.showModal && (
+                    <Portal onClose={this.handleCloseModal}>
+                    </Portal>
+                )}
             </div>
         )
     }
--- my-app/src/public-components/portal/portal.js
+++ my-app/src/public-components/portal/portal.js
@@ -5,28 +5,26 @@ import  './portal.scss';
 
 const portalRoot = document.getElementById('portal');
 
- export class Portal extends Component {
-   
+export class Portal extends Component {
     el = document.createElement('div');
     
     componentDidMount = () =>{
-        
-           portalRoot.appendChild(this.el);
+        portalRoot.appendChild(this.el);
     };
 
     componentWillUnmount = () =>{
         portalRoot.removeChild(this.el);
     };
-      
+
     render(){
         
         const { children , onClose } = this.props;
-         const content  = (
-         <div  className='modal'>
-             <button className="close-btn" onClick={onClose}>Close</button>
-             {children}
-         </div>
-         )
+        const content  = (
+            <div  className='modal'>
+                <button className="close-btn" onClick={onClose}>Close</button>
+                {children}
+            </div>
+        )
         return ReactDOM.createPortal(content,this.el)
     }
 }
